9f8a480604b35af4823fb31d7c31edc889e4fe39
Luiz Guilherme <jobs.lugui@gmail.com>
2021-02-16 00:59:16 -0300

    feat: boolean init

---
+++ logic-operators/scripts.js
@@ -0,0 +1,26 @@
+console.log('tipo: ', typeof 13)
+// typeof
+console.log('tipo: ', typeof 1.8)
+console.log('tipo: ', typeof -5)
+
+
+// operações aritméticas
+console.log('soma: ', 2 + 2)
+console.log('multiplicação: ', 2 * 4 - 3)
+console.log('divisão: ', 8 / 4)
+console.log('sub: ', 2 - 2)
+console.log('resto: ', 10 % 2)
+
+// Números especiais
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(typeof NaN)
+
+// Strings
+console.log(typeof 'lorem')
+console.log('lorem \n ipsum')
+console.log("My name is 'Slim Shady'")
+console.log(`2+2=${2+2}`) 
+
+// Booleans
+console.log(5 < 6)
